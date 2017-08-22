71c1147432b98a419eee785c0b5b953e84868556
Cain <350116542@qq.com>
2017-08-22 20:03:38 +0800

    添加Runtime工程

---
+++ RunTime/玩转iOS开发：iOS中的RunTime(一)/1.RunTime/1.RunTime/Module/RunTime/Controller/RunTimeController.m
@@ -0,0 +1,26 @@
+//
+//  RunTimeController.m
+//  1.RunTime
+//
+//  Created by Cain on 2017/8/21.
+//  Copyright © 2017年 Cain. All rights reserved.
+//
+
+#import "RunTimeController.h"
+#import "RunTimeModel.h"
+
+@interface RunTimeController ()
+
+@end
+
+@implementation RunTimeController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    self.view.backgroundColor = [UIColor whiteColor];
+    
+    RunTimeModel *model = [[RunTimeModel alloc] init];
+}
+
+@end
---
+++ RunTime/玩转iOS开发：iOS中的RunTime(一)/1.RunTime/1.RunTime/Module/RunTime/Model/RunTimeModel/RunTimeModel.h
@@ -0,0 +1,13 @@
+//
+//  RunTimeModel.h
+//  1.RunTime
+//
+//  Created by Cain on 2017/8/21.
+//  Copyright © 2017年 Cain. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface RunTimeModel : NSObject
+
+@end
